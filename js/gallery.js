ff57af7a517e34902d9134e9a6f032d574660094
clara <92735915+523Cs@users.noreply.github.com>
2023-02-17 11:44:46 -0500

    Add files via upload

--- js/gallery.js
+++ js/gallery.js
@@ -33,11 +33,46 @@ function animate() {
 /************* DO NOT TOUCH CODE ABOVE THIS LINE ***************/
 
 function swapPhoto() {
-	//Add code here to access the #slideShow element.
-	//Access the img element and replace its source
-	//with a new image from your images array which is loaded 
-	//from the JSON string
-	console.log('swap photo');
+	if(mCurrentIndex >= mImages.length){
+		mCurrentIndex = 0;
+	}
+	if (mCurrentIndex < 0) {
+		mCurrentIndex = mImages.length-1;
+	}
+//src is taking the photo and adding it to current index
+	document.getElementById('photo').src = mImages[mCurrentIndex].img;
+	var loc = document.getElementsByClassName('location');
+	loc[0].innerHTML = "Name: " + mImages[mCurrentIndex].location;
+
+	var des = document.getElementsByClassName('description');
+	des[0].innerHTML = "Age: " + mImages[mCurrentIndex].description;
+
+	var dt = document.getElementsByClassName('date');
+	dt[0].innerHTML = "Birth Year: " + mImages[mCurrentIndex].date;
+	
+	mLastFrameTime = 0
+	mCurrentIndex += 1
+}
+function prevPhoto() {
+	if (mCurrentIndex >= mImages.length) {
+		mCurrentIndex = 0;
+	}
+	if (mCurrentIndex < 0) {
+		mCurrentIndex = mImages.length - 1;
+	}
+	//src is taking the photo and adding it to current index
+	document.getElementById('photo').src = mImages[mCurrentIndex].img;
+	var loc = document.getElementsByClassName('location');
+	loc[0].innerHTML = "Name: " + mImages[mCurrentIndex].location;
+
+	var des = document.getElementsByClassName('description');
+	des[0].innerHTML = "Age: " + mImages[mCurrentIndex].description;
+
+	var dt = document.getElementsByClassName('date');
+	dt[0].innerHTML = "Birth Year: " + mImages[mCurrentIndex].date;
+
+	mLastFrameTime = 0
+	mCurrentIndex -= 1
 }
 
 // Counter for the mImages array
@@ -54,8 +89,32 @@ var mJson;
 
 // URL for the JSON to load by default
 // Some options for you are: images.json, images.short.json; you will need to create your own extra.json later
-var mUrl = 'insert_url_here_to_image_json';
+var mUrl = 'extra.json';
 
+// part 2 slideshow 1 and 2
+function fetchJSON() {
+	mRequest.onreadystatechange = function() {
+		console.log("on ready state change");
+		if(this.readyState == 4 && this.status == 200){
+			mJson = JSON.parse(mRequest.responseText);
+			iterateJSON(mJson)
+		}
+	}
+	mRequest.open("GET", mUrl, true);
+	mRequest.send();
+
+}
+
+function iterateJSON(mJson) {
+	for (x = 0; x < mJson.images.length; x++) {
+		mImages [x] = new GalleryImage();
+		mImages [x].location = mJson.images[x].imgLocation;
+		mImages[x].description = mJson.images[x].description;
+		mImages[x].date = mJson.images[x].date;
+		mImages[x].img = mJson.images[x].imgPath;
+
+	}		
+}
 
 //You can optionally use the following function as your event callback for loading the source of Images from your json data (for HTMLImageObject).
 //@param A GalleryImage object. Use this method for an event handler for loading a gallery Image object (optional).
@@ -67,10 +126,17 @@ function makeGalleryImageOnloadCallback(galleryImage) {
 }
 
 $(document).ready( function() {
-	
+	fetchJSON();
 	// This initially hides the photos' metadata information
-	$('.details').eq(0).hide();
-	
+	// $('.details').eq(0).hide();
+
+
+	//Come back to this!!!
+	$("#nextPhoto").position({
+		my: "right ",
+		at: "right center",
+		of: "#nav"
+	});
 });
 
 window.addEventListener('load', function() {
@@ -79,10 +145,15 @@ window.addEventListener('load', function() {
 
 }, false);
 
+// Assigning data from JSON list to variables that will be used in our slideshow
 function GalleryImage() {
+	var location = "";
+	var description = " ";
+	var date = " ";
+	var img = " ";
+}
 	//implement me as an object to hold the following data about an image:
 	//1. location where photo was taken
 	//2. description of photo
 	//3. the date when the photo was taken
 	//4. either a String (src URL) or an an HTMLImageObject (bitmap of the photo. https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement)
-}
